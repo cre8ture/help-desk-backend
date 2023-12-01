f6188a6c9d21469523256e3fa65594ba3acb32a5
cre8ture <bakukai@gmail.com>
2023-12-01 05:53:33 -0800

    new

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-const app = express();
 require('dotenv').config();
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Initialize the database
-// Initialize the database
+// Initialize the database connection without specifying the database
 const db = mysql.createConnection({
     host: process.env.RDS_HOSTNAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
-    database: process.env.RDS_DB_NAME
-  });
+    port: process.env.RDS_PORT
+});
 
 db.connect((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the MySQL database.');
-
-  // Create the tickets table if it doesn't exist
-  const query = `
-    CREATE TABLE IF NOT EXISTS tickets(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name TEXT NOT NULL,
-      email TEXT NOT NULL,
-      description TEXT NOT NULL,
-      date DATE NOT NULL,
-      time TIME NOT NULL,
-      status ENUM('new', 'in progress', 'resolved') NOT NULL DEFAULT 'new'
-    )
-  `;
-  db.query(query, (err, result) => {
     if (err) {
-      return console.error(err.message);
+        return console.error("ERROR", err.message);
     }
-    console.log('Tickets table created or already exists.');
-  });
+    console.log('Connected to the MySQL server.');
+
+    // Create the database if it doesn't exist
+    const createDbQuery = `CREATE DATABASE IF NOT EXISTS ${process.env.RDS_DB_NAME}`;
+    db.query(createDbQuery, (err, result) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Database created or already exists.');
+
+        // Switch to the database
+        db.changeUser({database : process.env.RDS_DB_NAME}, function(err) {
+            if (err) {
+                console.log('error in changing database', err);
+                return;
+            }
+
+            // Create the tickets table if it doesn't exist
+            const createTableQuery = `
+                CREATE TABLE IF NOT EXISTS tickets(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                description TEXT NOT NULL,
+                date DATE NOT NULL,
+                time TIME NOT NULL,
+                status ENUM('new', 'in progress', 'resolved') NOT NULL DEFAULT 'new'
+                )
+            `;
+            db.query(createTableQuery, (err, result) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log('Tickets table created or already exists.');
+            });
+        });
+    });
 });
+
 
 app.post('/tickets', (req, res) => {
   const { name, email, description } = req.body;
